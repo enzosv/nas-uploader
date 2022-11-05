aeda49eda730f4cd8ed17b11f511de33e6702352
enzosv <vergara.enzo@gmail.com>
2022-11-05 15:28:52 +0800

    fix upload status display

--- web/app.js
+++ web/app.js
@@ -11,11 +11,11 @@ async function getFiles() {
         data: "name",
         render: function (name, type, row) {
           const title = `${name} (${humanFileSize(row.size)})`;
-          if (row.progress) {
-            return `Uploading ${title} ${row.progress}%`;
-          }
           if (row.upload_id) {
             return `<a href=${row.path}>${title}</a>`;
+          }
+          if (row.upload_progress > 0) {
+            return `Uploading ${title} ${row.upload_progress}%`;
           }
           return `<button onclick="upload('${row.path}'); return false;">${title}</button>`;
         },
@@ -25,14 +25,23 @@ async function getFiles() {
 }
 
 async function upload(path) {
-  const response = await fetch("/upload?path=" + path);
+  const url = "/upload?path=" + path;
+  const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  if (data.message) {
+    const index = files.findIndex((x) => x.path === path);
+    var row = files[index];
+    row.upload_progress = 0.01;
+    files[index] = row;
+    $("#table").DataTable().row(index).data(row).invalidate();
+  }
 }
 
 function socket() {
   // Create WebSocket connection.
-  ws = new WebSocket("ws://localhost:8080/socket");
+  ws = new WebSocket(
+    `ws://${window.location.hostname}:${window.location.port}/socket`
+  );
   ws.addEventListener("open", function () {
     console.log("socket opened");
   });
@@ -49,7 +58,7 @@ function socket() {
       (x) => x.size === data.size && x.name === data.name
     );
     files[index] = data;
-    $("#table").DataTable().row(index).data(data).draw();
+    $("#table").DataTable().row(index).data(data).invalidate();
   });
 }
 
